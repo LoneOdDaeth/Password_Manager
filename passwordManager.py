179b858fac061fc1f8dbca96394eca94ea64d296
Selim Oğuz Şahin <selimoguz38@outlook.com>
2024-03-21 00:19:38 +0300

    v 1.0.1

--- passwordManager.py
+++ passwordManager.py
@@ -1,3 +1,4 @@
+# istenilen dosyadaki veriyi decrpt etmek için anahtarı da dosyaya kaydedip veriyi almamız lazım
 import os
 import random
 import string
@@ -24,21 +25,45 @@ def decryptedPassword(content): # decrypt the encrypted password
 
     return returnString
 
-def savePassword(platform, length):
+def savePassword(platform, length, key):
+
+    cypherKey = key.decode('utf-8')
+    printableData = cryptoPassword(length)
+
     with open(f"{platform}.key","a") as savePass:
-        savePass.write(cryptoPassword(length))
+        savePass.write(cypherKey + "\n" + printableData)
 
 def readPassword(platform):
+    lineList = []
     with open(f"{platform}.key","r") as readable:
-        willDecryptedPass = readable.read()
-    
-    return willDecryptedPass
+        for line in readable:
+            lineList.append(line.strip())
 
-key = Fernet.generate_key()
+    decryptedPass = lineList[1]
+    return decryptedPass
 
-length = int(input("What is the character length of the password to be created: "))
-platform = input("For which platform will the password be created: ")
+def showPassword(platform):
+        lineList = []
+        with open(f"{platform}.key","r") as readable:
+            for line in readable:
+                lineList.append(line.strip())
 
-print(savePassword(platform, length))
-willDecryptedPass = readPassword(platform)
-print(decryptedPassword(willDecryptedPass))
+        readableKey = lineList[0]
+        decryptedPass = lineList[1]
+
+        textOrigin = Fernet(readableKey).decrypt(decryptedPass)
+        return(textOrigin)
+
+choose = int(input("What do you want? Decrypted[1] or Creat the password[0]."))
+if choose == 0:
+    key = Fernet.generate_key()
+    length = int(input("What is the character length of the password to be created: "))
+    platform = input("For which platform will the password be created: ")
+
+    savePassword(platform, length, key)
+    willDecryptedPass = readPassword(platform)
+    print(decryptedPassword(willDecryptedPass))
+
+else:
+    file = input("Enter the file name: ")
+    print(showPassword(file))
