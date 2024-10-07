e78b6c3a688ae98a20b3db6deb3caa2cba0d886c
loneoddaeth <selimoguz38@outlook.com>
2024-10-07 17:19:10 +0300

    mainManage

--- Manage.py
+++ Manage.py
@@ -1,6 +1,20 @@
 import controlPanel as control
 import passwordManager as passManage
 
+def show_banner():
+    banner = """
+            **********************************************
+            *                                            *
+            *          WELCOME TO SAKHASAFE              *
+            *      Your Wisdom-Driven Security           *
+            *                                            *
+            **********************************************
+        """
+    print(banner)
+
 if __name__ == "__main__":
+    show_banner()    
     control.main()
     passManage.interface(control.username)
+
+    input("Program sona erdi. Çıkmak için bir tuşa basın...")
